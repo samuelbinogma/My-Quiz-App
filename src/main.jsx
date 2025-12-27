cbc5a95c9e4cfaa10971573ba232f5ac3dc6f59b
samuelbinogma <sbinogma41@gmail.com>
2025-12-27 21:27:06 +0000

    modified main.jsx for Routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './styles/global.css'
+import Home from './pages/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
